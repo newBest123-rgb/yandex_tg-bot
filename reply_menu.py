44444956d7d1440242805ab15490033ca1ce9111
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-04-23 21:13:25 +0300

    Delete reply_menu.py

--- reply_menu.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-kb_reply_menu = ReplyKeyboardMarkup(resize_keyboard=True)
-
-ib_reply_menu = KeyboardButton('Показать меню')
-
-kb_reply_menu.add(ib_reply_menu)
