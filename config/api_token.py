4b0ff3581ca5a505d4bb6069cca2baeb00fb27fd
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-04-23 20:02:19 +0300

    Add files via upload

---
+++ config/api_token.py
@@ -0,0 +1,8 @@
+TOKEN = '6999026276:AAHgXQAXgWM3ZPV9fxS-4pgBJr9dP6TcR0U'
+
+PARSE_MODE_HTML = 'HTML'
+
+URL_HOSPITAL = ('https://yandex.ru/maps/36/stavropol/?ll=41.939876%2C45.038466&mode=poi&poi%5Bpoint%5D='
+                '41.936264%2C45.039497&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1005208195&z=16.69')
+
+URL_GUIDE = 'https://skkdkb.ru/glavnaya/rukovodstvo.html'
