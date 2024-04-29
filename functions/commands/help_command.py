f39484c4d970eee4b0179a0ac2ea9f0a230f3906
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-04-29 18:58:58 +0300

    Delete functions directory

--- functions/commands/help_command.py
+++
@@ -1,9 +0,0 @@
-HELP_COMMAND = '''
-
-<b>/help</b> - <em>список комманд</em>
-<b>/start</b> - <em>начать работу с ботом</em>
-<b>/description</b> - <em>описание бота</em>
-<b>/menu</b> - <em>показать меню</em>
-<b>/contacts</b> - <em>показать контакты</em>
-
-'''
