18cd54d0bf3321a6696264cbb9b6150caefd9a66
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-04-29 18:55:54 +0300

    Update api_token.py

--- config/api_token.py
+++ config/api_token.py
@@ -6,3 +6,7 @@ URL_HOSPITAL = ('https://yandex.ru/maps/36/stavropol/?ll=41.939876%2C45.038466&m
                 '41.936264%2C45.039497&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1005208195&z=16.69')
 
 URL_GUIDE = 'https://skkdkb.ru/glavnaya/rukovodstvo.html'
+
+ADMIN_ID = '1762510156'
+
+SIGN_UP_ADMIN = 'Вы авторизовались как администратор!'
