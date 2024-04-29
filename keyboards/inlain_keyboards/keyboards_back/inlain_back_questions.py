03d37812cf778673a2f5b30b24ee75eec31a12f2
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-04-29 18:59:07 +0300

    Delete keyboards directory

--- keyboards/inlain_keyboards/keyboards_back/inlain_back_questions.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-ib_back = InlineKeyboardButton(text='⬅️Назад',
-                               callback_data='back')
-
-kb_back = InlineKeyboardMarkup()
-
-kb_back.add(ib_back)
