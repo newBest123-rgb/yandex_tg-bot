4e8a2d85a0725cee348b18ee20ab2de39a900957
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-05-01 17:00:10 +0300

    Update start_bot.py

--- start_bot/start_bot.py
+++ start_bot/start_bot.py
@@ -1,31 +1,24 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 
-from data_base.db_users.db_users import recording_users
 from data_base.db_add_users.db_add_users import id_users_admin
 from data_base.db_show_users.db_show_users import show_users
 
-from keyboards.inlain_keyboards.keyboards_menu.inlain_menu import ikb
-from keyboards.inlain_keyboards.keyboards_back.inlain_back_questions import kb_back
-from keyboards.inlain_keyboards.inlain_keyboards_admin.inlain_keyboards_admin_menu import ikb_admin
-
-from keyboards.reply_keyboards.keyboards_menu.reply_menu import kb_reply_menu
-
 from config.api_token import TOKEN
-from config.api_token import PARSE_MODE_HTML
-from config.api_token import ADMIN_ID
-from config.api_token import SIGN_UP_ADMIN
 
-from text.text_commands_bot.text_command_start import START_TEXT
-from text.text_commands_bot.text_command_description import DESCRIPTION_TEXT
-from text.text_commands_bot.text_command_contacts import TEXT_COMMAND_CONTACTS
-
-from functions.commands.help_command import HELP_COMMAND
 from functions.on_startup.on_startup import on_startup
+
+from functions.commands.contacts_commands import func_contacts_bot
+from functions.commands.description_commands import func_description_bot
+from functions.commands.help_commands import func_help_bot
+from functions.commands.menu_commands.pokaz_menu_commands import func_pokaz_menu_bot
+from functions.commands.menu_commands.menu_commands import func_start_menu_bot
+from functions.commands.start_commands.start_comands import func_start_start_bot
 
 from functions.callback.other.callback_aplication import callback_query_aplication
 from functions.callback.other.callback_paid_services import callback_query_paid_services
 from functions.callback.other.callback_contacts_rigestr import callback_query_contacts_rigestr
+
 from functions.callback.questions.callback_questions import callback_query_questions
 from functions.callback.questions.callback_first_questions import callback_query_first_questions
 from functions.callback.questions.callback_second_questions import callback_query_second_questions
@@ -34,11 +27,14 @@ from functions.callback.questions.callback_fourth_questions import callback_quer
 from functions.callback.questions.callback_fiveth_questions import callback_query_fiveth_questions
 from functions.callback.questions.callback_sixth_questions import callback_query_sixth_questions
 from functions.callback.questions.callback_seventh_questions import callback_query_seventh_questions
+
 from functions.callback.about_us.callback_about_us import callback_contacts_hospital
 from functions.callback.about_us.callback_about_us_contacts import callback_about_us_contacts
 from functions.callback.about_us.callback_about_us_guide import callback_about_us_guide
 from functions.callback.about_us.callback_about_us_home import callback_about_us_home
+
 from functions.callback.back.callback_back import callback_back_query_questions
+
 from functions.callback.admin.callback_admin_menu import callback_admin_menu
 from functions.callback.admin.callback_admin_panel_users import callback_admin_menu_back
 from functions.callback.admin.callback_admin_panel_user_id import callback_admin_users_id
@@ -49,34 +45,17 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start_start_bot(message: types.Message):
-    if message.from_user.id == int(ADMIN_ID):
-        await message.answer(text=SIGN_UP_ADMIN,
-                             reply_markup=ikb_admin)
-    else:
-        await bot.send_message(chat_id=message.from_user.id,
-                               text=START_TEXT,
-                               reply_markup=ikb,
-                               parse_mode=PARSE_MODE_HTML)
-
-    await recording_users(message)
+    await func_start_start_bot(message)
 
 
 @dp.message_handler(commands=['menu', 'Menu', 'MENU'])
 async def start_menu_bot(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text=START_TEXT,
-                           reply_markup=ikb,
-                           parse_mode=PARSE_MODE_HTML)
-    await message.delete()
+    await func_start_menu_bot(message)
 
 
-@dp.message_handler(Text('Показать меню', 'показать меню'))
+@dp.message_handler(Text('Показать меню'))
 async def pokaz_menu_bot(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text=START_TEXT,
-                           reply_markup=ikb,
-                           parse_mode=PARSE_MODE_HTML)
-    await message.delete()
+    await func_pokaz_menu_bot(message)
 
 
 @dp.message_handler(Text('Пользователи'))
@@ -114,37 +93,17 @@ async def callback_query(callback: types.CallbackQuery):
 
 @dp.message_handler(commands=['help', 'Help', 'HELP'])
 async def help_bot(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text=HELP_COMMAND,
-                           reply_markup=kb_reply_menu,
-                           parse_mode=PARSE_MODE_HTML)
-    await message.delete()
+    await func_help_bot(message)
 
 
 @dp.message_handler(commands=['description', 'Description', 'DESCRIPTION'])
 async def description_bot(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text=DESCRIPTION_TEXT,
-                           reply_markup=kb_back,
-                           parse_mode=PARSE_MODE_HTML)
-    await message.delete()
+    await func_description_bot(message)
 
 
 @dp.message_handler(commands=['contacts', 'Contacts', 'CONTACTS'])
 async def contacts_bot(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text=TEXT_COMMAND_CONTACTS,
-                           reply_markup=kb_back,
-                           parse_mode=PARSE_MODE_HTML)
-    await message.delete()
-
-
-@dp.message_handler(commands=['id', 'Id', 'ID'])
-async def id_bot(message: types.Message):
-    await message.answer(text=f'{message.from_user.id}',
-                         reply_markup=kb_back,
-                         parse_mode=PARSE_MODE_HTML)
-    await message.delete()
+    await func_contacts_bot(message)
 
 
 if __name__ == "__main__":
